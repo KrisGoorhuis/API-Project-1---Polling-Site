99afeb7ccdc4bddec2992f1601f6854cbfa1e68d
Kris Goorhuis <krisgoorhuis@gmail.com>
2017-02-20 15:16:39 -0800

    Progress on new poll construction and the voting mechanism

--- app/app.js
+++ app/app.js
@@ -4,16 +4,25 @@
    var app = angular.module('pollApp', ['ngRoute']);
    var getUrl = 'http://localhost:3000/getPolls';
    
-   app.controller("ViewController", function($http, $scope, $q) {
+   app.controller("ViewController", function($http, $scope) {
       var newThis = this;
-      newThis.polls = {};
-      this.currentPolls = {};
-      this.testVariable = {"number": 3};
+      newThis.polls = {}; // See performGet() for an explanation of this and the previous line.
       this.currentView = 1;
+      this.showingNameField = false;
+      this.inputFields = ["1", "2"]; // Start with two poll options!
+      var inputCount = 2; // Let us add a new number with each new option.
+      this.optionSelected;
+      
+      this.newPollTitle;
+      this.newPollOptions = {
+         // This is populated by the ng-model in creatPoll.html. Takes
+      }
+      this.selectedPoll = 0; // Governs which poll is shown in big.
       
       this.setView = function(newView) {       
          this.currentView = newView;
          this.updatePolls();
+         this.selectedPoll = 0;
       };
       
       this.checkView = function(matchingView) {
@@ -21,6 +30,8 @@
       };
       
       function performGet(callback) {
+         // Under a $http.get request, "this.polls" refers to a unique instance of  "this.polls". Changing it changes the copy, not the original.
+         // For some reason, updating the property ".polls" of a complete copy of "this" somehow updates the two-way binding Angular has with the original "this.polls" for use in the HTML. *shrugs*
          $http.get(getUrl).then(function(data) {
             newThis.polls = data["data"];
          });
@@ -30,7 +41,49 @@
          performGet();
       };
       
+      this.selectPoll = function(index) {
+         console.log(index);
+      }
       
+      this.vote = function() {
+         console.log(this.optionSelected);
+      }
+      
+      this.optionClicked = function(name) {
+         console.log(name);
+         this.optionSelected = name;
+      }
+      
+      
+      this.addPollOption = function() {  
+         inputCount++;
+         this.inputFields.push(inputCount); // Adds just a key. ng-model adds the value as it's typed in to the input field.
+      };
+
+      
+      this.transferPoll = function(selectedPoll) {
+         var pollOptions = this.polls[selectedPoll]["Poll Options"];
+         return pollOptions;
+      };
+    
+      this.submitPoll = function() {
+         $.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+            var geoipObject = JSON.parse(JSON.stringify(data, null, 2));
+            var ip = (geoipObject["ip"]); 
+            
+            // Again: just "this." refers to the get request's versions.
+            newThis.newPoll = {
+               "authorIP": ip,
+               "Poll Title": "newThis.newPollTitle",
+               "Poll Options": newThis.newPollOptions,
+               "Voter IPs": {}
+            }
+               console.log(newThis.newPoll);
+            // .put(I think) request goes here. Use .unshift instead of .push to put things on the front.
+            newThis.setView(1);
+         });  
+        
+      };
       
    });
 
@@ -41,11 +94,20 @@
       }
    }); 
 
-   app.directive('browse', function() {
+   app.directive('createPoll', function() {
       return {
          restrict: 'E',
-         templateUrl: '/assets/html/browse.html'
+         templateUrl: '/assets/html/createPoll.html'
       }
    });
+   
+   app.directive('selectedPoll', function() {
+      return {
+         restrict: 'E',
+         templateUrl: '/assets/html/selectedPoll.html'
+      }  
+   });
+   
+   
 })();
 
---
+++ database poll entry figuring.js
@@ -0,0 +1,149 @@
+{
+   "authorIP": "ip 1",
+   "Poll Title": "Best Number Under Four",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "One",
+         "Votes": 1
+      },
+      "Option 2 Values": {
+         "Text": "Two",
+         "Votes": 2
+      },
+      "Option 3 Values": {
+         "Text": "Three",
+         "Votes": 3
+      }
+   },
+   "Voter IPs": [{ // ARRAY, man!
+      100,
+      200,
+      300
+   }]
+},
+
+{
+   "authorIP": "ip 2",
+   "Poll Title": "Best Letter Past C",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "D",
+         "Votes": 1
+      },
+      "Option 2 Values": {
+         "Text": "E",
+         "Votes": 5
+      },
+      "Option 3 Values": {
+         "Text": "F",
+         "Votes": 3
+      }
+   },
+   "Voter IPs": {
+      1000,
+      2000,
+      3000
+   }
+},
+{
+   "authorIP": "ip 3",
+   "Poll Title": "Best Something Or Other",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "This",
+         "Votes": 5
+      },
+      "Option 2 Values": {
+         "Text": "That",
+         "Votes": 6
+      },
+      "Option 3 Values": {
+         "Text": "The Other",
+         "Votes": 7
+      }
+   },
+   "Voter IPs": {
+   
+   }
+}
+
+
+__________________
+
+
+{
+   "_id": "id #1",
+   "authorIP": "ip 1",
+   "authorName": "Kris",
+   "Poll Title": "Best Number Under Four",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "One",
+         "Votes": 1
+      },
+      "Option 2 Values": {
+         "Text": "Two",
+         "Votes": 2
+      },
+      "Option 3 Values": {
+         "Text": "Three",
+         "Votes": 3
+      }
+   },
+   "Voter IPs": [
+      100,
+      200,
+      300
+   ]
+},
+
+{
+   "_id": "id #2",
+   "authorIP": "ip 2",
+   "authorName": "Henk",
+   "Poll Title": "Best Letter Past C",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "D",
+         "Votes": 1
+      },
+      "Option 2 Values": {
+         "Text": "E",
+         "Votes": 5
+      },
+      "Option 3 Values": {
+         "Text": "F",
+         "Votes": 3
+      }
+   },
+   "Voter IPs": [
+      1000,
+      2000,
+      3000
+   ]
+},
+{
+   "_id": "id #3",
+   "authorIP": "ip 3",
+   "authorName": "Goorhuis",
+   "Poll Title": "Best Something Or Other",
+   "Poll Options": {
+      "Option 1 Values": {
+         "Text": "This",
+         "Votes": 5
+      },
+      "Option 2 Values": {
+         "Text": "That",
+         "Votes": 6
+      },
+      "Option 3 Values": {
+         "Text": "The Other",
+         "Votes": 7
+      }
+   },
+   "Voter IPs": [
+      5,
+      6,
+      7  
+   ]
+}
